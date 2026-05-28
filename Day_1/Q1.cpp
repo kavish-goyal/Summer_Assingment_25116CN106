807908a9b004edb777103933f0522634f1ca37d0
Kavish Goyal <kavishgoyal2006@gmail.com>
2026-05-28 22:50:28 +0530

    first upload

---
+++ Day_1/Q1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+
+    cout << "Enter N: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+
+    cout << "Sum of first " << n << " natural numbers is: " << sum;
+
+    return 0;
+}
---
+++ Day_1/Q4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, count = 0;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n == 0)
+    {
+        count = 1;
+    }
+    else
+    {
+        while (n != 0)
+        {
+            n = n / 10;
+            count++;
+        }
+    }
+
+    cout << "Number of digits is: " << count;
+
+    return 0;
+}
